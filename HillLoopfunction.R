553b06f15f749a7e084a06ebc5387f6486eef9fa
JosephGrilli <71455294+JosephGrilli@users.noreply.github.com>
2025-03-14 14:45:20 +0100

    Hill Fix
    Fixing missing term in Hill estimator function

---
+++ HillLoopfunction.R
@@ -0,0 +1,35 @@
+#' Hill parameter estimator
+#'
+#' @description Loops over dataset to find Hill parameter r that maximises the fit to the Pareto Type 1 distribution.
+#' @param x Data values used in parameter estimation.
+#' @param w Data weights used in estimation. Default = 1.
+#' @return Length of observation of x that should be used to combat bias in pareto Type 1 estimate.
+#' @export
+#' @examples
+#' \dontrun{
+#' HillLoopfunction(Value,Weights)
+#' }
+
+HillLoopfunction <- function(x,w){
+  simu <- data.frame("Value"=x,"Weights"=w)
+  n <- nrow(simu)
+  df.1 <- c(2,0)
+  for (r in round(n*0.05, digits=0):round(n*0.95, digits=0)) {
+    alpha.pt1.hat2 <- sum(simu$Weights[1:r])/(sum(simu$Weights[1:r]*(log(simu$Value[1:r]) - log(simu$Value[r+1]))))
+
+    CDF <- 1 - (simu$Value[r+1]/simu$Value[1:r])^alpha.pt1.hat2
+
+    dist <- aggregate(Weights ~ Value, simu[1:r,], sum)
+    dist <- dist[order(dist$Value,decreasing=FALSE),]
+    dist$ECDF <- (cumsum(dist$Weights)/sum(dist$Weights))
+
+    ECDF <-dist$ECDF[match(simu$Value[1:r],dist$Value)]
+
+    df <- c(-(cor(c(CDF), c(ECDF))^2), r)
+    if(!is.na(df[1])){
+      if (df[1]<=df.1[1]) {df.1 <-df}
+    }
+  }
+  r <- df.1[2]
+  return(r)
+}
